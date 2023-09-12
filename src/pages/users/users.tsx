8787ff175cd29b8f3f7f7cae68166ecfdc08504a
seena <iamsrinivas230@gmail.com>
2023-09-12 18:37:12 +0530

    updated code

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,6 +1,6 @@
 import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/dataTable";
-import "./Users.scss";
+import "./users.scss";
 import { useState } from "react";
 import Add from "../../components/add/add";
 import { userRows } from "../../data";
